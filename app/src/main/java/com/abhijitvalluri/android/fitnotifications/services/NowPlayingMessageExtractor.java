877342f0d49b44ff7333ff2d0341f3f936e1f7cb
benjymous <rm.github@grapefruitopia.com>
2017-04-19 21:35:23 +0100

    Only handle media locations, to ignore any other messages from media apps
    e.g. "Now downloading" type messages

--- app/src/main/java/com/abhijitvalluri/android/fitnotifications/services/NowPlayingMessageExtractor.java
+++ app/src/main/java/com/abhijitvalluri/android/fitnotifications/services/NowPlayingMessageExtractor.java
@@ -1,8 +1,10 @@
 package com.abhijitvalluri.android.fitnotifications.services;
 
+import android.app.Notification;
 import android.os.Bundle;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Message Extractor that formats "Now playing" notifications into a sensible message
@@ -23,12 +25,17 @@ public class NowPlayingMessageExtractor extends GenericMessageExtractor {
     public CharSequence[] getTitleAndText(String appPackageName, Bundle extras, int notificationFlags) {
         CharSequence[] titleAndText = super.getTitleAndText(appPackageName, extras, notificationFlags);
 
-        String text = "";
-        if (songNameFirst) {
-            text = titleAndText[0].toString() + " - " + titleAndText[1].toString();
-        }
-        else {
-            text = titleAndText[1].toString() + " - " + titleAndText[0].toString();
+        Set<String> keys = extras.keySet();
+
+        String text = null;
+
+        if (keys.contains(Notification.EXTRA_MEDIA_SESSION)) {
+            // Only handle media notifiations, to ignore any other spam
+            if (songNameFirst) {
+                text = titleAndText[0].toString() + " - " + titleAndText[1].toString();
+            } else {
+                text = titleAndText[1].toString() + " - " + titleAndText[0].toString();
+            }
         }
         String prevNotificationText = mNotificationStringMap.put(appPackageName, text);
         // TODO: add more specific checks to avoid blocking legitimate identical notifications
